e87072b49e366f5f7e45bf617fab8acf7fbf1cdd
lthihuongn <lilou.boutibonnes@student.junia.com>
2026-06-10 10:06:20 +0200

    init + config  ok

--- Ressources/main.c
+++ Ressources/main.c
@@ -6,64 +6,178 @@
 
 #include <xc.h>
 
-// Configuration materielle du PIC :
-#pragma config FEXTOSC = OFF           // Pas de source d'horloge externe
-#pragma config RSTOSC = HFINTOSC_64MHZ // Horloge interne de 64 MHz
-#pragma config WDTE = OFF              // Désactiver le watchdog
+#pragma config FEXTOSC = OFF
+#pragma config RSTOSC = HFINTOSC_64MHZ
+#pragma config WDTE = OFF
 
-#define _XTAL_FREQ 64000000 // Frequence d'horloge - necessaire aux macros de delay (_delay(N) ; __delay_us(N) ; __delay_ms(N)))
+#define _XTAL_FREQ 64000000
 
-// Définition des masques, macros, etc. :
-// TODO
+#define MASK_LED0 0x01
+#define MASK_LED1 0x02
+#define MASK_LED2 0x04
+#define MASK_LED3 0x08
+#define MASK_LED4 0x10
+#define MASK_LED5 0x20
+#define MASK_LED6 0x40
+#define MASK_LED7 0x80
 
+#define MASK_BP0 0x01
+#define MASK_BP1 0x02
 
-// Déclaration de fonctions et variables globales permettant au code C et à l'asm de les partager
-// Une même fonction ou variable côté asm est préfixée par un underscore, et ne l'est pas côté C
-// Avec ce formalisme, elles sont utilisables de façon intercangeable et transparente :
-// | ---- asm ----- | ------------- C ----------------- |
-// | _TX_64LEDS  <--|--> void TX_64LEDS(void)           |
-// | _pC         <--|--> volatile const char * pC       |
-// | _LED_MATRIX <--|--> volatile char LED_MATRIX [256] |
+// Canaux ADC
+#define CH_ENV1 0x05  // RA5
+#define CH_ENV2 0x04  // RA4
+#define CH_ENV3 0x03  // RA3
+#define CH_ENV4 0x02  // RA2
 
-// Définition des fonctions relatives à la matrice de LEDs:
-extern void TX_64LEDS(void); // Fonction définie dans tx.asm ; Fonction permettant d'envoyer la commande pour piloter les 64 LEDs, telle que décrite dans LED_MATRIX
+// Couleurs RGBW
+#define ENV1_R 255
+#define ENV1_G   0
+#define ENV1_B   0
+#define ENV1_W   0
 
-// Définition des constantes / variables relatives à la matrice de LEDs :
-volatile char LED_MATRIX [256] ; // Definition d'une matrice de 64 x 4 octets contenant les composantes R/G/B/W de chaque LED (1 octet/couleur/LED)
-volatile const char * pC = LED_MATRIX; // Pointeur vers LED_MATRIX
+#define ENV2_R   0
+#define ENV2_G 255
+#define ENV2_B   0
+#define ENV2_W   0
 
+#define ENV3_R   0
+#define ENV3_G   0
+#define ENV3_B 255
+#define ENV3_W   0
 
-// - Fonction main ----------------------------------------------------------------------
+#define ENV4_R 128
+#define ENV4_G 128
+#define ENV4_B 128
+#define ENV4_W 255
+
+// Interface C <-> ASM
+extern void TX_64LEDS(void);
+extern void TX_1LEDS(void);
+extern void TX_1BYTE(void);
+extern void TX_1BIT(void);
+extern void READ_1BYTE(void);
+
+volatile char        LED_MATRIX[256];
+volatile const char *pC = LED_MATRIX;
+
+// ------------------------------------------------------------------
+// select_channel() : selectionne le canal ADC
+// ------------------------------------------------------------------
+void select_channel(char channel) {
+    ADPCH = channel;
+    __delay_us(10);
+}
+
+// ------------------------------------------------------------------
+// ad_read() : lance une conversion et retourne ADRESH
+// ------------------------------------------------------------------
+char ad_read(void) {
+    ADCON0 |= 0x02;           // ADGO = 1
+    while (ADCON0 & 0x02) {} // attend fin de conversion
+    return ADRESH;
+}
+
+// ------------------------------------------------------------------
+// ad_read_max() : lit 200 fois et retourne le maximum
+//                pour capturer le pic du signal audio
+// ------------------------------------------------------------------
+char ad_read_max(void) {
+    char max_val = 0;
+    char val;
+    int i;
+    for (i = 0; i < 200; i++) {
+        val = ad_read();
+        if ((unsigned char)val > (unsigned char)max_val) {
+            max_val = val;
+        }
+        __delay_us(50);
+    }
+    return max_val;
+}
+
+// ------------------------------------------------------------------
+// get_thermometre() : convertit valeur ADC (128-255) en code
+//                    thermometre sur 8 bits
+// ------------------------------------------------------------------
+char get_thermometre(char adc_val) {
+    int val    = (unsigned char)adc_val;  // 128..255
+    int niveau = (val - 128) * 8 / 127;  // 0..8
+    char code  = 0;
+    char i;
+    for (i = 0; i < niveau; i++) {
+        code = (char)((code << 1) | 0x01);
+    }
+    return code;
+}
+
+// ------------------------------------------------------------------
+// fill_row() : remplit une ligne de 8 LEDs dans LED_MATRIX
+// ------------------------------------------------------------------
+void fill_row(unsigned char row, char therm,
+              char R, char G, char B, char W) {
+    unsigned char col;
+    for (col = 0; col < 8; col++) {
+        unsigned int idx = (unsigned int)(row * 8 + col) * 4;
+        if (therm & (1 << col)) {
+            LED_MATRIX[idx + 0] = R;
+            LED_MATRIX[idx + 1] = G;
+            LED_MATRIX[idx + 2] = B;
+            LED_MATRIX[idx + 3] = W;
+        } else {
+            LED_MATRIX[idx + 0] = 0;
+            LED_MATRIX[idx + 1] = 0;
+            LED_MATRIX[idx + 2] = 0;
+            LED_MATRIX[idx + 3] = 0;
+        }
+    }
+}
+
+// ------------------------------------------------------------------
+// main()
+// ------------------------------------------------------------------
 void main(void) {
-    /* Configuration des entrées / sorties */
-    // TODO
+    // RA2..RA5 en entrees analogiques
+    TRISA  = 0x3C;   // RA2,RA3,RA4,RA5 en entree
+    ANSELA = 0x3C;   // RA2..RA5 analogiques
 
-    /* Corps du programme */
-    // TODO
+    // LED de test RC1 en sortie
+    TRISC &= ~MASK_LED1;
 
-    /* Code pour vérification du bon fonctionnement de la partir uC (à retirer par la suite) : === DEMO CODE */
+    // Configuration ADC
+    ADCON1 = 0b01100000;  // Fosc/64, Vref = AVdd/AVss
+    ADCON0 = 0b00000001;  // ADON=1, ADGO=0
 
-    // Initialisation des LEDs =================================================================== DEMO CODE
-    TRISB &= 0xEF; // LED_MASTER : OUTPUT -------------------------------------------------------- DEMO CODE
-    TRISC &= 0x00; // LED0-7     : OUTPUT -------------------------------------------------------- DEMO CODE
+    char therm;
 
-    LATB &= 0xEF; // Eteindre LEDM   ------------------------------------------------------------- DEMO CODE
-    LATC  = 0x00; // Eteindre LED0-7 ------------------------------------------------------------- DEMO CODE
+    while (1) {
+        // ENV1 -> lignes 0 et 1 (rouge)
+        select_channel(CH_ENV1);
+        therm = get_thermometre(ad_read_max());
+        fill_row(0, therm, ENV1_R, ENV1_G, ENV1_B, ENV1_W);
+        fill_row(1, therm, ENV1_R, ENV1_G, ENV1_B, ENV1_W);
 
-    // Blink sur LEDM : ========================================================================== DEMO CODE
-    LATB |= 0x10;    // Allumer LEDM   ----------------------------------------------------------- DEMO CODE
-    __delay_ms(500); // Macro de délai ----------------------------------------------------------- DEMO CODE
-    LATB &= 0xEF;    // Eteindre LEDM  ----------------------------------------------------------- DEMO CODE
-    __delay_ms(500); // Macro de délai ----------------------------------------------------------- DEMO CODE
-    LATB |= 0x10;    // Allumer LEDM   ----------------------------------------------------------- DEMO CODE
+        // ENV2 -> lignes 2 et 3 (vert)
+        select_channel(CH_ENV2);
+        therm = get_thermometre(ad_read_max());
+        fill_row(2, therm, ENV2_R, ENV2_G, ENV2_B, ENV2_W);
+        fill_row(3, therm, ENV2_R, ENV2_G, ENV2_B, ENV2_W);
 
-    // Chenillard : ============================================================================== DEMO CODE
-    while(1){ //---------------------------------------------------------------------------------- DEMO CODE
-        for (int i=0; i<8; i++){ // -------------------------------------------------------------- DEMO CODE
-            LATC = 0x01 << i;    // Commander les LEDs de test sur le PORTC ---------------------- DEMO CODE
-            __delay_ms(125);     // Macro de délai ----------------------------------------------- DEMO CODE
-        } // ------------------------------------------------------------------------------------- DEMO CODE
-    } // ----------------------------------------------------------------------------------------- DEMO CODE
+        // ENV3 -> lignes 4 et 5 (bleu)
+        select_channel(CH_ENV3);
+        therm = get_thermometre(ad_read_max());
+        fill_row(4, therm, ENV3_R, ENV3_G, ENV3_B, ENV3_W);
+        fill_row(5, therm, ENV3_R, ENV3_G, ENV3_B, ENV3_W);
 
-    return;
+        // ENV4 -> lignes 6 et 7 (blanc)
+        select_channel(CH_ENV4);
+        therm = get_thermometre(ad_read_max());
+        fill_row(6, therm, ENV4_R, ENV4_G, ENV4_B, ENV4_W);
+        fill_row(7, therm, ENV4_R, ENV4_G, ENV4_B, ENV4_W);
+
+        // Envoi vers la matrice LED
+        TX_64LEDS();
+
+        __delay_ms(50);
+    }
 }
--- Ressources/tx.asm
+++ Ressources/tx.asm
@@ -1,42 +1,386 @@
 #include <xc.inc>
 
-; When assembly code is placed in a psect, it can be manipulated as a
-; whole by the linker and placed in memory.
-psect   txfunc,local,class=CODE,reloc=2 ; PIC18's should have a reloc (alignment) flag of 2 for any psect which contains executable code.
+psect   txfunc,local,class=CODE,reloc=2
 
 ; -----------------------------------------------------------------
 ; GLOBALS
-;
-; Déclaration de fonctions et variables globales permettant au code C et à l'asm de les partager
-; Une même fonction ou variable côté asm est préfixée par un underscore, et ne l'est pas côté C
-; Avec ce formalisme, elles sont utilisables de façon intercangeable et transparente :
 ; | ---- asm ----- | ------------- C ----------------- |
 ; | _TX_64LEDS  <--|--> void TX_64LEDS(void)           |
 ; | _pC         <--|--> volatile const char * pC       |
 ; | _LED_MATRIX <--|--> volatile char LED_MATRIX [256] |
+global _TX_64LEDS
+global _pC
+global _LED_MATRIX
 
-; Fonction globales
-global _TX_64LEDS ; Fonction définie dans tx.asm ; Fonction permettant d'envoyer la commande pour piloter les 64 LEDs, telle que décrite dans LED_MATRIX
+; -----------------------------------------------------------------
+; VARIABLES EN RAM (acces direct, bank 0)
+; -----------------------------------------------------------------
+psect   txdata,local,class=BANK0,space=1,noexec
+tx_octet:   DS 1    ; octet en cours de transmission
 
-; Constantes/variables globales
-global _pC         ; Constante définie dans main.c ; Pointeur vers LED_MATRIX
-global _LED_MATRIX ; Variable  définie dans main.c ; Tableau (256 octets = 64 x 4) des composantes RGBW de la matrice LED (1 octet/couleur/LED)
+psect   txfunc,local,class=CODE,reloc=2
 
+; =================================================================
+; TX_0 : envoie un bit 0 sur CMD_MATRIX (RB5)
+;   Signal haut court  (~300 ns = 5 NOP a 64MHz)
+;   Signal bas  long   (~900 ns = 14 NOP a 64MHz)
+; =================================================================
+TX_0:
+    banksel LATB
+    bsf     LATB, 5         ; RB5 = 1 (haut)
+    nop
+    nop
+    nop
+    nop
+    nop
+    bcf     LATB, 5         ; RB5 = 0 (bas)
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    return
+
+; =================================================================
+; TX_1 : envoie un bit 1 sur CMD_MATRIX (RB5)
+;   Signal haut long   (~600 ns = 10 NOP a 64MHz)
+;   Signal bas  court  (~600 ns = 9  NOP a 64MHz)
+; =================================================================
+TX_1:
+    banksel LATB
+    bsf     LATB, 5         ; RB5 = 1 (haut)
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    bcf     LATB, 5         ; RB5 = 0 (bas)
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    return
+
+; =================================================================
+; TX_1BIT : envoie le bit de poids fort de tx_octet
+;           puis decale tx_octet d un cran vers la gauche
+; =================================================================
+TX_1BIT:
+    banksel tx_octet
+    btfsc   tx_octet, 7     ; si bit7 = 0, saute la ligne suivante
+    goto    TX1BIT_send1
+    call    TX_0
+    goto    TX1BIT_shift
+TX1BIT_send1:
+    call    TX_1
+
+TX1BIT_shift:
+    banksel tx_octet
+    ; bit7 <- bit6
+    btfsc   tx_octet, 6
+    goto    TX1BIT_b7_1
+    bcf     tx_octet, 7
+    goto    TX1BIT_b6
+TX1BIT_b7_1:
+    bsf     tx_octet, 7
+TX1BIT_b6:
+    ; bit6 <- bit5
+    btfsc   tx_octet, 5
+    goto    TX1BIT_b6_1
+    bcf     tx_octet, 6
+    goto    TX1BIT_b5
+TX1BIT_b6_1:
+    bsf     tx_octet, 6
+TX1BIT_b5:
+    ; bit5 <- bit4
+    btfsc   tx_octet, 4
+    goto    TX1BIT_b5_1
+    bcf     tx_octet, 5
+    goto    TX1BIT_b4
+TX1BIT_b5_1:
+    bsf     tx_octet, 5
+TX1BIT_b4:
+    ; bit4 <- bit3
+    btfsc   tx_octet, 3
+    goto    TX1BIT_b4_1
+    bcf     tx_octet, 4
+    goto    TX1BIT_b3
+TX1BIT_b4_1:
+    bsf     tx_octet, 4
+TX1BIT_b3:
+    ; bit3 <- bit2
+    btfsc   tx_octet, 2
+    goto    TX1BIT_b3_1
+    bcf     tx_octet, 3
+    goto    TX1BIT_b2
+TX1BIT_b3_1:
+    bsf     tx_octet, 3
+TX1BIT_b2:
+    ; bit2 <- bit1
+    btfsc   tx_octet, 1
+    goto    TX1BIT_b2_1
+    bcf     tx_octet, 2
+    goto    TX1BIT_b1
+TX1BIT_b2_1:
+    bsf     tx_octet, 2
+TX1BIT_b1:
+    ; bit1 <- bit0
+    btfsc   tx_octet, 0
+    goto    TX1BIT_b1_1
+    bcf     tx_octet, 1
+    goto    TX1BIT_b0
+TX1BIT_b1_1:
+    bsf     tx_octet, 1
+TX1BIT_b0:
+    ; bit0 <- 0
+    bcf     tx_octet, 0
+    return
+
+; =================================================================
+; READ_1BYTE : lit l octet pointe par FSR0 dans tx_octet
+;              et incremente FSR0
+; =================================================================
+READ_1BYTE:
+    movf    POSTINC0, 0, 0  ; WREG = *FSR0, FSR0++
+    banksel tx_octet
+    movwf   tx_octet        ; tx_octet = WREG
+    return
+
+; =================================================================
+; TX_1BYTE : envoie 8 bits d un octet (MSB en premier)
+; =================================================================
+TX_1BYTE:
+    call    READ_1BYTE      ; charge *FSR0 dans tx_octet, FSR0++
+    call    TX_1BIT         ; bit 7
+    call    TX_1BIT         ; bit 6
+    call    TX_1BIT         ; bit 5
+    call    TX_1BIT         ; bit 4
+    call    TX_1BIT         ; bit 3
+    call    TX_1BIT         ; bit 2
+    call    TX_1BIT         ; bit 1
+    call    TX_1BIT         ; bit 0
+    return
+
+; =================================================================
+; TX_1LED : envoie les 4 octets R, G, B, W d une LED
+; =================================================================
+TX_1LED:
+    call    TX_1BYTE        ; R
+    call    TX_1BYTE        ; G
+    call    TX_1BYTE        ; B
+    call    TX_1BYTE        ; W
+    return
+
+; =================================================================
+; _TX_64LEDS : envoie les 64 LEDs de LED_MATRIX
+; =================================================================
 _TX_64LEDS:
-    ; Cette fonction envoie sur CMD_MATRIX l'intégralité de la matrice LED_MATRIX,
-    ; Chaque bit de chaque octet encodé en largeur d'impulsion
+    ; --- Test : allume LED6 (RC6) pour verifier que la fonction est appelee ---
+    banksel TRISC
+    bcf     TRISC, 6        ; RC6 en sortie
+    banksel LATC
+    bsf     LATC, 6         ; RC6 = 1 -> LED6 allumee
 
-    ; Place un pointeur au début de la matrice LED_MATRIX
-    ; Voir section 10.8.12 (p. 150) de la datasheet PIC18F25K40
-    MOVFF _pC + 0, WREG ; Charge le LSB du pointeur de LED_MATRIX dans WREG
-    MOVWF FSR0L, 0      ; Définit le LSB du registre d'adressage indirect
-    MOVFF _pC + 1, WREG ; Charge le MSB du pointeur de LED_MATRIX dans WREG
-    MOVWF FSR0H, 0      ; Définit le MSB du registre d'adressage indirect
+    ; --- Configure RB5 (CMD_MATRIX) en sortie numerique ---
+    banksel TRISB
+    bcf     TRISB, 5        ; RB5 en sortie
+    banksel ANSELB
+    bcf     ANSELB, 5       ; RB5 numerique
 
-    ; Désormais, dès l'exécution de l'instruction suivante, la valeur pointée par <FSR0H-FSR0L> est chargée dans WREG, et <FSR0H-FSR0L> est incrémenté :
-    ; MOVF POSTINC0, 0, 0
+    ; --- Charge l adresse de LED_MATRIX dans FSR0 via _pC ---
+    MOVFF   _pC + 0, WREG
+    MOVWF   FSR0L, 0
+    MOVFF   _pC + 1, WREG
+    MOVWF   FSR0H, 0
 
-    ; Envoie la commande pour piloter chacune des 64 LEDs
-    ; TODO
+    ; --- Envoie les 64 LEDs (boucle deroulee) ---
+    call    TX_1LED     ; LED  1
+    call    TX_1LED     ; LED  2
+    call    TX_1LED     ; LED  3
+    call    TX_1LED     ; LED  4
+    call    TX_1LED     ; LED  5
+    call    TX_1LED     ; LED  6
+    call    TX_1LED     ; LED  7
+    call    TX_1LED     ; LED  8
+    call    TX_1LED     ; LED  9
+    call    TX_1LED     ; LED 10
+    call    TX_1LED     ; LED 11
+    call    TX_1LED     ; LED 12
+    call    TX_1LED     ; LED 13
+    call    TX_1LED     ; LED 14
+    call    TX_1LED     ; LED 15
+    call    TX_1LED     ; LED 16
+    call    TX_1LED     ; LED 17
+    call    TX_1LED     ; LED 18
+    call    TX_1LED     ; LED 19
+    call    TX_1LED     ; LED 20
+    call    TX_1LED     ; LED 21
+    call    TX_1LED     ; LED 22
+    call    TX_1LED     ; LED 23
+    call    TX_1LED     ; LED 24
+    call    TX_1LED     ; LED 25
+    call    TX_1LED     ; LED 26
+    call    TX_1LED     ; LED 27
+    call    TX_1LED     ; LED 28
+    call    TX_1LED     ; LED 29
+    call    TX_1LED     ; LED 30
+    call    TX_1LED     ; LED 31
+    call    TX_1LED     ; LED 32
+    call    TX_1LED     ; LED 33
+    call    TX_1LED     ; LED 34
+    call    TX_1LED     ; LED 35
+    call    TX_1LED     ; LED 36
+    call    TX_1LED     ; LED 37
+    call    TX_1LED     ; LED 38
+    call    TX_1LED     ; LED 39
+    call    TX_1LED     ; LED 40
+    call    TX_1LED     ; LED 41
+    call    TX_1LED     ; LED 42
+    call    TX_1LED     ; LED 43
+    call    TX_1LED     ; LED 44
+    call    TX_1LED     ; LED 45
+    call    TX_1LED     ; LED 46
+    call    TX_1LED     ; LED 47
+    call    TX_1LED     ; LED 48
+    call    TX_1LED     ; LED 49
+    call    TX_1LED     ; LED 50
+    call    TX_1LED     ; LED 51
+    call    TX_1LED     ; LED 52
+    call    TX_1LED     ; LED 53
+    call    TX_1LED     ; LED 54
+    call    TX_1LED     ; LED 55
+    call    TX_1LED     ; LED 56
+    call    TX_1LED     ; LED 57
+    call    TX_1LED     ; LED 58
+    call    TX_1LED     ; LED 59
+    call    TX_1LED     ; LED 60
+    call    TX_1LED     ; LED 61
+    call    TX_1LED     ; LED 62
+    call    TX_1LED     ; LED 63
+    call    TX_1LED     ; LED 64
 
-    RETURN
+    ; --- Reset : RB5 bas pendant > 80 us pour valider la trame ---
+    banksel LATB
+    bcf     LATB, 5         ; RB5 = 0
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 10
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 20
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 30
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 40
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 50
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 60
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 70
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 80
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 90
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop  ; 100
+
+    return
+
+    END
